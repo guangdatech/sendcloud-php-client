bdec2d52871b51c2a2e5f0f74db64b684abf581e
Edward Yang <yangjin@opencart.cn>
2021-08-27 09:00:13 +0800

    fixed send single and templete

--- src/SendCloud.php
+++ src/SendCloud.php
@@ -9,13 +9,22 @@
  * @created    2021-08-26 20:55:39
  * @modified   2021-08-26 20:55:39
  */
+
+namespace Guangda\SendCloud;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
+
 class SendCloud
 {
     const BASE_URI = 'https://api.sendcloud.net/apiv2/';
 
-    protected $apiUser;
-
-    protected $apiKey;
+    private $apiUser;
+    private $apiKey;
+    private $from;
+    private $fromName;
+    private $client;
 
     /**
      * @param string $apiUser
@@ -25,6 +34,10 @@ class SendCloud
     {
         $this->apiUser = $apiUser;
         $this->apiKey = $apiKey;
+        $this->client = new Client([
+            'base_uri' => self::BASE_URI,
+            'timeout' => 5.0
+        ]);
     }
 
     /**
@@ -37,13 +50,100 @@ class SendCloud
         return new self($apiUser, $apiKey);
     }
 
-    public function send()
+    /**
+     * 设置发送来源邮件
+     *
+     * @param $fromEmail
+     * @return $this
+     */
+    public function setFrom($fromEmail): SendCloud
     {
-        echo 11;
+        $this->from = $fromEmail;
+        return $this;
     }
 
-    public function sendTemplate()
+    /**
+     * 设置发送来源名称
+     *
+     * @param $fromName
+     * @return $this
+     */
+    public function setFromName($fromName): SendCloud
     {
+        $this->fromName = $fromName;
+        return $this;
+    }
 
+    /**
+     * 发送普通邮件
+     * @see https://www.sendcloud.net/doc/email_v2/send_email/#_1
+     *
+     * @param $data
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function sendMail($data): ResponseInterface
+    {
+        $uri = 'mail/send';
+        if (is_array($data['to'])) {
+            $to = implode(';', $data['to']);
+        } else {
+            $to = $data['to'];
+        }
+        $useAddressList = (isset($data['useAddressList']) && $data['useAddressList']) ? 'true' : 'false';
+        $param = array(
+            'apiUser' => $this->apiUser,
+            'apiKey' => $this->apiKey,
+            'from' => $this->from,
+            'fromName' => $this->fromName,
+            'to' => $to,
+            'subject' => $data['subject'] ?? '',
+            'plain' => $data['plain'] ?? '',
+            'html' => $data['html'] ?? '',
+            'respEmailId' => 'true',
+            'useAddressList' => $useAddressList
+        );
+
+        $requestData = [
+            'header' => 'Content-Type: application/x-www-form-urlencoded',
+            'form_params' => $param
+        ];
+        return $this->client->post($uri, $requestData);
+    }
+
+    /**
+     * 通过 SendCloud 模板发送邮件
+     * @see https://www.sendcloud.net/doc/email_v2/send_email/#_2
+     *
+     * @param $data
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function sendTemplate($data): ResponseInterface
+    {
+        $uri = 'mail/sendtemplate';
+        if (is_array($data['to'])) {
+            $to = implode(';', $data['to']);
+        } else {
+            $to = $data['to'];
+        }
+        $useAddressList = (isset($data['useAddressList']) && $data['useAddressList']) ? 'true' : 'false';
+        $param = array(
+            'apiUser' => $this->apiUser,
+            'apiKey' => $this->apiKey,
+            'from' => $this->from,
+            'fromName' => $this->fromName,
+            'to' => $to,
+            'subject' => $data['subject'] ?? '',
+            'templateInvokeName' => $data['templateInvokeName'] ?? '',
+            'contentSummary' => $data['contentSummary'] ?? '',
+            'useAddressList' => $useAddressList
+        );
+
+        $requestData = [
+            'header' => 'Content-Type: application/x-www-form-urlencoded',
+            'form_params' => $param
+        ];
+        return $this->client->post($uri, $requestData);
     }
 }
